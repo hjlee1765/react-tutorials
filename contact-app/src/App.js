d9c1839489ba760f0583291ecf9a4c7edce02919
hjlee1765 <hjlee1765@gmail.com>
2018-07-04 21:56:04 +0900

    자식 컴포넌트에서 부모 컴포넌트 state 변경

--- contact-app/src/App.js
+++ contact-app/src/App.js
@@ -1,21 +1,37 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import PhoneForm from './components/PhoneForm';
 
 class App extends Component {
+
+  //데이터가 들어갈 때 마다, 고유한 id값이 들어가게 한다.
+  id = 0;
+
+  state = {
+    information: [],
+  }
+
+  handleCreate = (data) =>{
+    //비 구조화 할당 - 코드 가독성, 간결하게.
+    const { information } = this.state;
+
+    this.setState({
+      //concat : 기존에 있던 배열은 놔두고, 새로운 배열을 추가해서 리턴함.
+      information: information.concat({
+        ...data,
+        id: this.id ++
+      })
+    })
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <PhoneForm onCreate = {this.handleCreate}/>
+        {JSON.stringify(this.state.information)}
       </div>
     );
   }
 }
 
 export default App;
+
--- contact-app/src/components/PhoneForm.js
+++ contact-app/src/components/PhoneForm.js
@@ -1,13 +1,53 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class PhoneForm extends Component {
-    render() {
-        return (
-            <div>
-                
-            </div>
-        );
-    }
+  state = {
+    name: "",
+    phone: ""
+  };
+
+  handleChange = e => {
+    this.setState({
+      // onChange 이벤트를 행한 target의 name attr의 값을 가져온다.
+      [e.target.name]: e.target.value
+    });
+  };
+
+  handleSubmit = e => {
+    // form에서 submit을 하면 postback이 되는 HTML속성이 있음.
+    // 그 작업(페이지의 리로딩) 막아주는 역할을 함.
+    e.preventDefault();
+
+    this.props.onCreate({
+      name: this.state.name,
+      phone: this.state.phone,
+    })
+
+    this.setState({
+      name:'',
+      phone:'',
+    })
+  };
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <input
+          name="name"
+          placeholder="이름"
+          onChange={this.handleChange}
+          value={this.state.name}
+        />
+        <input
+          name="phone"
+          placeholder="전화번호"
+          onChange={this.handleChange}
+          value={this.state.phone}
+        />
+        <button type="submit">등록</button>
+      </form>
+    );
+  }
 }
 
 export default PhoneForm;
